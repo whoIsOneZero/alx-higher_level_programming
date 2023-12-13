ac0659c22289812686c824f8889ef27f674b40f1
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-12-13 22:16:22 +0000

    list number of records with the same score

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- Lists the number of record with the same score
-SELECT age COUNT(*) AS number
-FROM second_table
-GROUP BY score
-GROUP BY number DESC;
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+GROUP BY `number` DESC;
