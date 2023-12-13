771a04712df1c7d8bd9ebe0858aa65ba2fdb40d7
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-12-13 19:26:10 +0000

    insert a new row to the 'first_table' table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Inserts a new row in the table `first_table`
--- db name passed as an arg of `mysql` command
+-- database name passed as an arg of `mysql` command
 
 INSERT INTO first_table (id, name) VALUES (89, "Best School")
