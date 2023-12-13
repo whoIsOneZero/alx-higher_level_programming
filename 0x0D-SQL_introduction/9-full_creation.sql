60ec0c05214823be2b7194e7db6a56c12be0bbdf
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-12-13 19:55:48 +0000

    create table and add records

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Creates a table in the current database
+-- db name passed as an argument of the `mysql` command
+
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+    ); 
+
+INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "Bob", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
