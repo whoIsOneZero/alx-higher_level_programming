70ecf1bc0fc288536b739d96b45919886a32f0c7
Shallom Ezekiel <oh.shalom.0@gmail.com>
2023-12-13 16:42:20 +0000

    create a table in the current database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -2,4 +2,3 @@
 -- db name passed as an argument of the `mysql` command
 
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256)); 
--- CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
