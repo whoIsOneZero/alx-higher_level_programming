2bbab240906052b8bf06aa4e5494ddeaab6e353b
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-01-11 00:22:09 +0000

    task 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let num = 0;
+  list.forEach(listElement => {
+    if (listElement === searchElement) {
+      num++;
+    }
+  });
+  return (num);
+}
