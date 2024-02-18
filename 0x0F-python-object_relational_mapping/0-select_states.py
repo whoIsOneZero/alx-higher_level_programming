9fa4b42d8e769529da4de36515ae326abe9f087e
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-02-18 14:10:03 -0800

    task 1: check

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" list all the states from the database specified"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for state in rows:
+        print(state)
+    cur.close()
+    db.close()
