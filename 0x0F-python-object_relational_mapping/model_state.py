4bcb94f41b2aaa4b89d78472c0cfe59f7518f98c
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-03-19 08:46:05 -0700

    add doc strings

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-
+"""
+Contains State class and Base, an instance of declarative_base()
+"""
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,6 +10,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    Class with id and name attributes of each state
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
