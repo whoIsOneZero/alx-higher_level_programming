9d75dab13631d0d9b9e1ee22fec1062f15da588d
Shallom Ezekiel <oh.shalom.0@gmail.com>
2024-03-25 17:56:09 +0000

    rmv old project files

--- 0x0F-python-object_relational_mapping/README.md
+++
@@ -1,2 +0,0 @@
-# Object-Relational Mapping
-Good to go.
--- 0x12-javascript-warm_up/7-multi_c.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-  console.log('Missing number of occurrences');
-} else {
-  const num = parseInt(process.argv[2]);
-  for (let i = 0; i < num; i++) {
-    console.log('C is fun');
-  }
-}
